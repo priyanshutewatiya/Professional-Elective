dc887f099f8671432b8a9cf923dcff5bf594fbc9
unknown <teotiapriyanshu1@gmail.com>
2026-02-02 16:16:58 +0530

    Intial commit

---
+++ Tutorial-1/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+    <h1>Hello Kiet </h1>
+    <h2>Priyanshu</h2>
+    </div>
+  )     
+}
+export default App  
---
+++ demo.js
@@ -0,0 +1,45 @@
+// const arr = ["HTML","CSS","JS"]
+// arr.push("python")
+
+// console.log(arr)
+
+const sales = "Toyota"
+function carType(name){
+    return name === "Honda" ? name : "Sorry we dont sell this car"
+}
+
+const car = {
+    carn : "City",
+    getCar: carType("Honda"), Special : sales
+}
+// console.log(car.carn)
+// console.log(car.getCar)
+// console.log(car.Special)
+
+const color = ["red","blue","green"]
+
+// color.forEach((i) => console.log(i))
+
+// const arr = [1,3,4,5]
+// arr.forEach((i,a) => console.log(i+3,a))
+
+// for(let i =0 ; i< arr.length;i++){
+//     arr[i] += 3
+// }
+// console.log(arr)
+
+// for(const i in color){
+//     console.log(i , color[i])
+// }
+
+// const user = {
+//     name : "Rabins",
+//     age : 25,
+//     Hobby : "Coding"
+// }
+// for(const index in user){
+//     console.log(index , user[index])
+// }
+
+
+
